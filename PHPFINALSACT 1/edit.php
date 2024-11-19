f44a2cffe9cfd164673ea5244837e01ab34b2acc
jebsonpp <114137142+jebsonpp@users.noreply.github.com>
2024-11-20 02:52:22 +0800

    Add files via upload

---
+++ PHPFINALSACT 1/edit.php
@@ -0,0 +1,39 @@
+<?php require_once 'core/handling.php'; ?>
+<?php require_once 'core/models.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Edit</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<?php $getUserByID = getUserByID($pdo, $_GET['id']); ?>
+	<h1>Edit Here!</h1>
+	<form action="core/handleForms.php?id=<?php echo $_GET['id']; ?>" method="POST">
+		<p>
+			<label for="firstName">First Name</label> 
+			<input type="text" name="first_name" value="<?php echo $getUserByID['first_name']; ?>">
+		</p>
+		<p>
+			<label for="firstName">Last Name</label> 
+			<input type="text" name="last_name" value="<?php echo $getUserByID['last_name']; ?>">
+		</p>
+		<p>
+			<label for="firstName">Email</label> 
+			<input type="text" name="email" value="<?php echo $getUserByID['email']; ?>">
+		</p>
+		<p>
+			<label for="firstName">Gender</label> 
+			<input type="text" name="gender" value="<?php echo $getUserByID['gender']; ?>">
+		</p>
+		<p>
+			<label for="firstName">position</label> 
+			<input type="text" name="position" value="<?php echo $getUserByID['position']; ?>">
+		</p>
+			<input type="submit" value="Save" name="editUserBtn">
+		</p>
+	</form>
+</body>
+</html>
---
+++ PHPFINALSACT 1/insert.php
@@ -0,0 +1,39 @@
+<?php require_once 'core/handling.php'; ?>
+<?php require_once 'core/models.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Insert</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<h1>Edit the user!</h1>
+	<form action="core/handling.php" method="POST">
+		<p>
+			<label for="firstName">First Name</label> 
+			<input type="text" name="first_name">
+		</p>
+		<p>
+			<label for="firstName">Last Name</label> 
+			<input type="text" name="last_name">
+		</p>
+		<p>
+			<label for="firstName">Email</label> 
+			<input type="text" name="email">
+		</p>
+		<p>
+			<label for="firstName">Gender</label> 
+			<input type="text" name="gender">
+		</p>
+		<p>
+			<label for="firstName">position</label> 
+			<input type="text" name="position">
+		</p>
+		<p>
+			<input type="submit" name="insertUserBtn">
+		</p>
+	</form>
+</body>
+</html>
